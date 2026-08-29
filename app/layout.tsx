01d09dfa1ac8f74c771bce9c7e5004bcc8818d65
Nana Opoku Gyamfi Asante <nanasante2000@gmail.com>
2026-08-29 12:53:06 +0000

    feat(seo): add direct HTML head link tags for Bing and search crawlers

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,8 +97,12 @@ export default function RootLayout({
       className={`${inter.variable} h-full antialiased`}
     >
       <head>
-        <link rel="icon" href="/logo.png" />
-        <link rel="apple-touch-icon" href="/logo.png" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" type="image/png" sizes="48x48" href="/favicon-48x48.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
 
         {/* Search Engine Verification Tags */}
         {process.env.NEXT_PUBLIC_GOOGLE_SITE_VERIFICATION && (
